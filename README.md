1103726aa1d7a0fad12de9831ea496d597fc49d9
unknown <lekhanareddy1296@gmail.com>
2019-05-08 12:56:17 +0530

    applied beautification for all files

--- README.md
+++ README.md
@@ -2,36 +2,39 @@
 
 ## Table of Contents
 
-- [Instructions](#instructions)
-- [Information](#project details)
-- [Game Running](#running)
-- [Game Playing](#how to play)
+-   [Instructions](#instructions)
+-   [Information](<#project details>)
+-   [Game Running](#running)
+-   [Game Playing](<#how to play>)
 
 ## Instructions
 
 Use this [rubric](https://review.udacity.com/#!/rubrics/15/view) for self-checking.
 
-##INFORMATION
-* Clone the repository from the udacity that contains the details and files
-   1. style.css
-   2. index.html
-   3. js (app, engine, resorces)
-   4. images
+\##INFORMATION
+
+-   Clone the repository from the udacity that contains the details and files
+    1.  style.css
+    2.  index.html
+    3.  js (app, engine, resorces)
+    4.  images
 
 Make sure the functions you write are **object-oriented** - either class functions (like `Player` and `Enemy`) or class prototype functions such as `Enemy.prototype.checkCollisions`. Also make sure that the keyword `this` is used appropriately within your class and class prototype functions to refer to the object the function is called upon.
 
 ## Game Running
-1. Created a constructor for Enemy class and player class.
-2. Created an object for all enemy objects in the form of array as allEnemies.
-3. Created an object for the player object in a variable called player.
-4. initially set positions of player and enemies.
-5. for every movement of player set coordinates for appropriate key.
-6. Speed of enemies are taken randomly in which the game runs at the same speed for all computers.
-7. Speed of enemies are taken randomly in which the game runs at the same speed for all computers.
+
+1.  Created a constructor for Enemy class and player class.
+2.  Created an object for all enemy objects in the form of array as allEnemies.
+3.  Created an object for the player object in a variable called player.
+4.  initially set positions of player and enemies.
+5.  for every movement of player set coordinates for appropriate key.
+6.  Speed of enemies are taken randomly in which the game runs at the same speed for all computers.
+7.  Speed of enemies are taken randomly in which the game runs at the same speed for all computers.
 
 ## Game Playing
-1. player moves corresponding direction by pressing a key(left, right, up, down) until it does not move away from specified width and height.
-2. If player move to water level then player is reset to initial position.
-3. If player and enemies collide with each other then player reset to its initial position.
+
+1.  player moves corresponding direction by pressing a key(left, right, up, down) until it does not move away from specified width and height.
+2.  If player move to water level then player is reset to initial position.
+3.  If player and enemies collide with each other then player reset to its initial position.
 
 For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
--- js/app.js
+++ js/app.js
@@ -1,39 +1,39 @@
-var Enemy = function(x,y,speed) {
-  this.x=x;
-  this.y=y;
-  this.speed=speed;
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Enemy = function(x, y, speed) {
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = 'images/enemy-bug.png';
 };
 //constructor
 class Player {
-  constructor(x,y) {
-    this.x=x;
-    this.y=y;
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
     this.sprite = 'images/char-boy.png';
   };
 }
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-  this.x=this.x+this.speed*dt;
-  if(this.x==0){
-    this.speed=100+Math.floor(Math.random()*100);
+  this.x = this.x + this.speed * dt;
+  if (this.x == 0) {
+    this.speed = 100 + Math.floor(Math.random() * 100);
   }
-  if(this.x>505){
-    this.x=0;
+  if (this.x > 505) {
+    this.x = 0;
   }
-  if(this.x+60>player.x && this.x<player.x+65 && this.y+60>player.y && this.y<player.y+70){
-    player.x=200;
-    player.y=410;
+  if (this.x + 60 > player.x && this.x < player.x + 65 && this.y + 60 > player.y && this.y < player.y + 70) {
+    player.x = 200;
+    player.y = 410;
   }
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
 };
 // Player.prototype.update = function(dt) {
 //
@@ -41,7 +41,7 @@ Enemy.prototype.update = function(dt) {
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 Player.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -57,27 +57,27 @@ Player.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies=[];
-var forEnemy=[60,145,230];
-var player= new Player(200,410);
-for(i in forEnemy){
-  var obj=new Enemy(0,forEnemy[i],150);
+var allEnemies = [];
+var forEnemy = [60, 145, 230];
+var player = new Player(200, 410);
+for (i in forEnemy) {
+  var obj = new Enemy(0, forEnemy[i], 150);
   allEnemies.push(obj);
 }
-player.handleInput=function(key){
-  if(key=="left" && this.x>0)
-    this.x-=101;
- if(key=="right" && this.x<303)
-    this.x+=101;
-  if(key=="up" && this.y>0)
-    this.y-=85;
-  if(key=="down" && this.y<400)
-  this.y+=85;
-  if(this.y<60){
-    setTimeout(()=>{
-      this.x=200;
-      this.y=410;
-    },1000);
+player.handleInput = function(key) {
+  if (key == "left" && this.x > 0)
+    this.x -= 101;
+  if (key == "right" && this.x < 303)
+    this.x += 101;
+  if (key == "up" && this.y > 0)
+    this.y -= 85;
+  if (key == "down" && this.y < 400)
+    this.y += 85;
+  if (this.y < 60) {
+    setTimeout(() => {
+      this.x = 200;
+      this.y = 410;
+    }, 1000);
   }
 }
 
@@ -85,12 +85,12 @@ player.handleInput=function(key){
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
